782ad6ffca840d4f91863d8eee124216157fb323
Abhishek Kumar <meisabikumar@gmail.com>
2022-11-21 09:18:53 +0530

    Update chatroom.php

--- chatroom.php
+++ chatroom.php
@@ -132,7 +132,7 @@ $user_data = $user_object->get_user_all_data();
 					</div>
 				</div>
 
-				<form  id="chat_form" data-parsley-errors-container="#validation_error">
+				<form id="chat_form" data-parsley-errors-container="#validation_error">
 					<div class="input-group mb-3">
 						<textarea class="form-control" id="chat_message" name="chat_message"
 							placeholder="Type Message Here" data-parsley-maxlength="1000"
@@ -292,90 +292,5 @@ $user_data = $user_object->get_user_all_data();
 	}).mount('#app')
 </script>
 
-<script type="text/javascript">
-
-	$(document).ready(function () {
-
-		// var conn = new WebSocket('ws://localhost:8080');
-		// conn.onopen = function (e) {
-		// 	console.log("Connection established!");
-		// };
-
-		// conn.onmessage = function (e) {
-		// 	console.log(e.data);
-
-		// 	var data = JSON.parse(e.data);
-
-		// 	var row_class = '';
-
-		// 	var background_class = '';
-
-		// 	if (data.from == 'Me') {
-		// 		row_class = 'row justify-content-end';
-		// 		background_class = 'text-dark alert-light';
-		// 	}
-		// 	else {
-		// 		row_class = 'row justify-content-start';
-		// 		background_class = 'alert-success';
-		// 	}
-
-		// 	var html_data = "<div class='" + row_class + "'><div class='col-sm-10'><div class='shadow-sm alert " + background_class + "'><b>" + data.from + " - </b>" + data.msg + "<br /><div class='text-right'><small><i>" + data.dt + "</i></small></div></div></div></div>";
-
-		// 	$('#messages_area').append(html_data);
-
-		// 	$("#chat_message").val("");
-		// };
-
-		// $('#chat_form').parsley();
-
-		// $('#messages_area').scrollTop($('#messages_area')[0].scrollHeight);
-
-		// $('#chat_form').on('submit', function (event) {
-
-		// event.preventDefault();
-
-		// if ($('#chat_form').parsley().isValid()) {
-
-		// 	var user_id = $('#login_user_id').val();
-
-		// 	var message = $('#chat_message').val();
-
-		// 	var data = {
-		// 		userId: user_id,
-		// 		msg: message,
-		// 		command: 'group',
-		// 	};
-
-		// 	conn.send(JSON.stringify(data));
-
-		// 	$('#messages_area').scrollTop($('#messages_area')[0].scrollHeight);
-
-		// }
-
-		// });
-
-		// $('#logout').click(function () {
-
-		// user_id = $('#login_user_id').val();
-
-		// $.ajax({
-		// 	url: "action.php",
-		// 	method: "POST",
-		// 	data: { user_id: user_id, action: 'leave' },
-		// 	success: function (data) {
-		// 		var response = JSON.parse(data);
-
-		// 		if (response.status == 1) {
-		// 			conn.close();
-		// 			location = 'index.php';
-		// 		}
-		// 	}
-		// })
-
-		// });
-
-	});
-
-</script>
 
 </html>
